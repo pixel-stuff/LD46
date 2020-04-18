45cc80413e373fcbeadcb2e13ca2b38b5bfe4e68
Mathias <mussetmathias@gmail.com>
2020-04-19 01:20:32 +0200

    Created first version of ouija

---
+++ PixelStuff-LD46/Assets/Scripts/OuijaDataDisplayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OuijaDataDisplayer : MonoBehaviour {
+
+  [SerializeField] Text textA;
+  [SerializeField] Text textF;
+  [SerializeField] Text textO;
+
+
+  public void Display(Vector3 data) {
+    textA.text = "Amplitude: " + data.x;
+    textF.text = "Frequence: " + data.y;
+    textO.text = "Offset: " + data.z;
+  }
+}
